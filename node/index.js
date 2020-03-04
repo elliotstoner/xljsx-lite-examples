1db2607465235971df5972b7da189c68f3258e37
Elliot Stoner <estoner@captechconsulting.com>
2020-03-04 16:07:16 -0500

    Add Node example

---
+++ node/index.js
@@ -0,0 +1,62 @@
+const { xljsxLite } = require('xljsx-lite');
+
+// Create a helper function to create the cells
+const generateCell = value => ({
+	value,
+	// In a future release I'd like to automatically determine the type for users, like this
+	type: isNaN(value) ? 'string' : 'number'
+});
+
+// Create a helper function to create the rows
+const generateRow = (...cells) => ([...cells]);
+
+const sheet1 = {
+	// If we don't create a sheetName, xljsx-lite will generate the standard "Sheet 1" for us
+	data: [
+		generateRow(
+			generateCell('First Name'),
+			generateCell('Last Name'),
+			generateCell('Favorite Number')
+		),
+		generateRow(
+			generateCell('Elliot'),
+			generateCell('Stoner'),
+			generateCell(83)
+		),
+		generateRow(
+			generateCell('Kiva'),
+			generateCell('Gayle'),
+			generateCell(2)
+		),
+		generateRow(
+			generateCell('Chad'),
+			generateCell('Florida'),
+			generateCell(9)
+		)
+	]
+};
+
+const sheet2 = {
+	sheetName: 'Famous Numbers',
+	data: [
+		generateRow(
+			generateCell('More'),
+			generateCell('Data'),
+			generateCell('Here')
+		),
+		generateRow(
+			generateCell(3.1415),
+			generateCell(2.7182),
+			generateCell('6.0221515 x 10^23'),
+			generateCell('186,282'),
+			generateCell('i')
+		)
+	]
+};
+
+const config = {
+	filename: 'Example XLJSX file',
+	sheets: [sheet1, sheet2]
+};
+
+xljsxLite(config);
